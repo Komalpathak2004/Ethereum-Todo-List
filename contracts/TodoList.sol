89e9dd25d674d56b70134a2d8dfc6b8e15e8312b
Komal Pathak <164751240+Komalpathak2004@users.noreply.github.com>
2024-06-30 21:31:53 +0530

    Add files via upload

---
+++ contracts/TodoList.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.5.0;
+
+contract TodoList {
+    uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    event TaskCreated(uint id, string content, bool completed);
+    event TaskCompleted(uint id, bool completed);
+
+    constructor() public {
+        createTask("Check out dappuniversity.com");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
+    }
+
+    function toggleCompleted(uint _id) public {
+        Task memory task = tasks[_id];
+        task.completed = !task.completed;
+        tasks[_id] = task;
+        emit TaskCompleted(_id, task.completed);
+    }
+}
